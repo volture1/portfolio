d9aa999da921854d91f47b5f0a002cf9552b8ebd
Tobias Ostergren <ostergrentobias@gmail.com>
2022-06-03 20:09:27 +0200

    template without routes

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import InfoBox from './components/infobox.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <InfoBox></InfoBox>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/infobox.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const infobox = () => {
+  return (
+    <div> <p>Welcome to my website!</p></div>
+  )
+}
+
+export default infobox
